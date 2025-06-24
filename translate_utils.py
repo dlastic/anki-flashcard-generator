7b8ff3537d8e7a652ae4e68116967f4e02c180af
dlastic <daniellastic@gmail.com>
2025-06-24 22:02:53 +0200

    Remove DeepL translation function

--- translate_utils.py
+++ translate_utils.py
@@ -1,21 +1,7 @@
-import os
-
-import deepl
 from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
-
-
-def translate_sentence_deepl(sentence: str, target_lang: str):
-    """Translate sentence using DeepL translator"""
-    DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
-    if not DEEPL_API_KEY:
-        raise NameError("The API_KEY not found")
-    translator = deepl.Translator(DEEPL_API_KEY)
-    result = translator.translate_text(sentence, target_lang=target_lang)
-
-    return result.text  # type: ignore
 
 
 def translate_sentence_chatgpt(sentence: str) -> str:
@@ -30,8 +16,4 @@ def translate_sentence_chatgpt(sentence: str) -> str:
     return response.output_text
 
 
-print(
-    translate_sentence_chatgpt(
-        "Das erkennst du an ihrem Gang? Wow."
-    )
-)
+print(translate_sentence_chatgpt("Das erkennst du an ihrem Gang? Wow."))
