b1e56b54532061bf51682d027251a16ecbbb4128
dlastic <daniellastic@gmail.com>
2025-06-28 19:30:35 +0200

    Add translated_content initialization and validation in main function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
-from notion_utils import get_page_content
 from general_utils import print_seperating_line
+from notion_utils import get_page_content
+from translate_utils import translate_sentences_chatgpt
 
 
 def main() -> None:
@@ -14,6 +15,19 @@ def main() -> None:
 
     print_seperating_line()
     print("Page content loaded successfully.")
+
+    translated_content = [
+        ["yapping", "Why are you <u>yapping</u> at me?"],
+        ["rot", "I thought you'd leave me to <u>rot</u> here."],
+        ["something to eat", "Can I get <u>something to eat</u>?"],
+        ["screw loose", "You have just as much of a <u>screw loose</u> as I do, man."],
+        ["gasping", "Then he starts off <u>gasping</u>."],
+    ]
+    print_seperating_line()
+    if translated_content is None:
+        sys.exit("Exiting: No sentences to translate.")
+
+    print("Translation completed successfully.")
 
 
 if __name__ == "__main__":
