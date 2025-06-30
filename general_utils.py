d3dbbd97434dadad4bc0385a403ae50ecf70a186
dlastic <daniellastic@gmail.com>
2025-06-30 21:52:08 +0200

    Move print_seperating_line function into main script

--- general_utils.py
+++
@@ -1,2 +0,0 @@
-def print_seperating_line() -> None:
-    print("--------------------------------------------")
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import sys
 
-from general_utils import print_seperating_line
 from generator import generate_flashcards
 from notion_utils import get_page_content
 from translate_utils import translate_sentences_chatgpt
@@ -11,10 +10,9 @@ def main() -> None:
     """Generate anki cloze deck from sentences in the Notion page."""
     title = sys.argv[1]
     content = get_page_content(title)
-    if content is None:
-        print_seperating_line()
-        sys.exit(f"Exiting: No page found with title: {title}")
     print_seperating_line()
+    if content is None:
+        sys.exit(f"Exiting: No page found with title: {title}")
     print("Page content loaded successfully.")
 
     translated_content = translate_sentences_chatgpt(content)
@@ -26,6 +24,10 @@ def main() -> None:
     flashcards = generate_flashcards(content, json.loads(translated_content))
     print_seperating_line()
     print("Flashcards generated successfully.")
+
+
+def print_seperating_line() -> None:
+    print("--------------------------------------------")
 
 
 if __name__ == "__main__":
