c5de3378cbae9bebc3318d07840f906cfca6dcb9
dlastic <daniellastic@gmail.com>
2025-06-30 21:48:00 +0200

    Update main function to use dynamic translation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
 import sys
 
 from general_utils import print_seperating_line
+from generator import generate_flashcards
 from notion_utils import get_page_content
 from translate_utils import translate_sentences_chatgpt
 
@@ -12,22 +14,18 @@ def main() -> None:
     if content is None:
         print_seperating_line()
         sys.exit(f"Exiting: No page found with title: {title}")
-
     print_seperating_line()
     print("Page content loaded successfully.")
 
-    translated_content = [
-        ["yapping", "Why are you <u>yapping</u> at me?"],
-        ["rot", "I thought you'd leave me to <u>rot</u> here."],
-        ["something to eat", "Can I get <u>something to eat</u>?"],
-        ["screw loose", "You have just as much of a <u>screw loose</u> as I do, man."],
-        ["gasping", "Then he starts off <u>gasping</u>."],
-    ]
+    translated_content = translate_sentences_chatgpt(content)
     print_seperating_line()
     if translated_content is None:
         sys.exit("Exiting: No sentences to translate.")
-
     print("Translation completed successfully.")
+
+    flashcards = generate_flashcards(content, json.loads(translated_content))
+    print_seperating_line()
+    print("Flashcards generated successfully.")
 
 
 if __name__ == "__main__":
