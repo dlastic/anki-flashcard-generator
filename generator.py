2c64aab07b6a63e6a4229c41ce9a81138c9deef2
Daniel Láštic <daniellastic@gmail.com>
2025-09-14 14:00:15 +0200

    Refactor flashcard generation for new input format

--- generator.py
+++ generator.py
@@ -1,7 +1,9 @@
-import re
 import logging
+import re
 
 import genanki
+
+from translate_utils import TranslationItem
 
 
 class FlashcardGenerationError(Exception):
@@ -20,33 +22,25 @@ def convert_underline_to_cloze(sentence: str) -> str:
     return re.sub(r"<u>(.*?)</u>", r"{{c1::\1}}", sentence)
 
 
-def generate_flashcards(
-    content: list[str], translated_content: list[list[str]]
-) -> list[str]:
+def generate_flashcards(translated_content: list[TranslationItem]) -> list[str]:
     """Generate a list of flashcards"""
-    content_len = len(content)
-    translated_content_len = len(translated_content)
-    if content_len != translated_content_len:
-        raise FlashcardGenerationError(
-            f"Content length ({content_len}) and translated content length ({translated_content_len}) do not match."
+    flashcards = []
+    for translation in translated_content:
+        words_source, sentence_source, sentence_target = (
+            translation.words_source,
+            translation.sentence_source,
+            translation.sentence_target,
         )
 
-    flashcards = []
-    for target_line, translation in zip(content, translated_content):
-        if not (isinstance(translation, (list, tuple)) and len(translation) == 2):
+        if "<u>" not in sentence_target or "</u>" not in sentence_target:
             logger.warning(
-                f"Skipping flashcard due to invalid translation format: {translation}"
+                f"Skipping flashcard due to missing underline tags in: {sentence_target}"
             )
             continue
-        if "<u>" not in target_line or "</u>" not in target_line:
-            logger.warning(
-                f"Skipping flashcard due to missing underline tags in: {target_line}"
-            )
-            continue
-        source_word, source_sentence = translation
-        target_line_cloze = convert_underline_to_cloze(target_line)
+
+        sentence_target_cloze = convert_underline_to_cloze(sentence_target)
         flashcards.append(
-            f"<i>{source_word}</i><br><i>{source_sentence}</i><br>{target_line_cloze}"
+            f"<i>{words_source}</i><br><i>{sentence_source}</i><br>{sentence_target_cloze}"
         )
 
     return flashcards
